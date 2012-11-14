bf408b83809645ba4eab820e69969b2d441771bb
Graham Jenson <grahamjenson@maori.geek.nz>
2012-11-14 16:21:32 +1300

    fixed what was broke, made the ranking more solid

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -7,6 +7,33 @@ class Playlist < ActiveRecord::Base
 
   has_many :plitems
 
+  def orderedplitems(session)
+    #THIS METHOD IS VERRY INEFFICIENT
+    #get plitems
+    plis = self.plitems
+    #seperate into two lists those that have been ranked and those that havnt
+    beenRanked = plis.select{ |x| x.rank(session) }
+    notRanked = plis.select{ |x| not x.rank(session) }
+
+    #filter already removed or aggregated to be removed
+    notRemoved = beenRanked.select{|x| x.rank(session) >= 0}
+    notRanked = notRanked.select{|x| x.rank >= 0}
+
+    #sort both lists
+    notRanked = notRanked.sort{ |pl1,pl2| pl1.rank - pl2.rank}
+    notRemoved = notRemoved.sort{ |pl1,pl2| pl1.rank(session) - pl2.rank(session)}
+
+    #create ranks for not ranked
+    i = beenRanked.size()
+    for nr in notRanked do
+      br = nr.buildRank(session,i)
+      br.save()
+      i += 1
+    end
+
+    return notRemoved + notRanked
+  end
+
   def generate_uuid
     self.uuid ||= rand(36**8).to_s(36)
   end
--- app/models/plitem.rb
+++ app/models/plitem.rb
@@ -1,7 +1,7 @@
 class Plitem < ActiveRecord::Base
   belongs_to :playlist
 
-  attr_accessible :youtubeid, :title, :thumbnail, :length, :rating
+  attr_accessible :youtubeid, :title, :thumbnail, :length
 
   validates :youtubeid, :title, :playlist, :presence => true
 
@@ -19,15 +19,33 @@ class Plitem < ActiveRecord::Base
     errors.add(:playlist, "Must be Unique") if playlist.plitems.find{|pli| pli != self and pli.youtubeid == youtubeid}
   end
 
-  def aggregateRating
+  def find_plitem_rank(session)
+    plitem_ranks.select{|x| x.session_id == session.id}.first
+  end
+  #get the rank of the item
+  def rank(session = nil)
+    if session
+      #if there is a session we return the rank
+      plrank = self.find_plitem_rank(session)
+      if plrank
+        return plrank.rank
+      else
+        return nil
+      end
+    else
+      #no session return the average rank
+      return aggregateRank
+    end
+  end
+
+
+  def aggregateRank
     #main ranking function
     removes = plitem_ranks.select{|r| r.rank < 0}.size()
-
-    ranks = plitem_ranks.select{|r| r.rank >= 0}.map{|r| r.rank}
-
+    ranks =  plitem_ranks.select{|r| r.rank >= 0}.map{|r| r.rank}
     total = plitem_ranks.size
 
-    #25% of pple want it removed it is removed
+    #25% of pple want it removed it is removed, can come back with more pple
     if removes > (0.25 * total)
       return -1
     else
@@ -35,22 +53,10 @@ class Plitem < ActiveRecord::Base
     end
   end
 
-  def setAggregateRating
-    self.rating = aggregateRating()
+  def buildRank(session,rank)
+    return plitem_ranks.build(:session => session, :rank => rank)
   end
 
-  def findOrCreatePlitemRank(session)
-    plrank = plitem_ranks.where(:session_id => session.id).first
-    if not plrank
-      plrank = plitem_ranks.create(:session => session, :rank => aggregateRating())
-    end
-    return plrank
-  end
-
-  def setRatingForSession(session)
-    plrank = findOrCreatePlitemRank(session)
-    self.rating = plrank.rank
-  end
 
   def as_json(options)
     jsond = super(options)
